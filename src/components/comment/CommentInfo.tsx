a17f83ec3bc7bec78443baf6dadef36dce759ba3
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-01-24 15:23:47 +0100

    add "you" badge to own comments

--- src/components/comment/CommentInfo.tsx
+++ src/components/comment/CommentInfo.tsx
@@ -1,23 +1,34 @@
+import { useContext } from 'react'
 import * as types from '../../types'
+import { UserContext } from '../CommentsSection'
 
 export function CommentInfo({
   info,
 }: {
   info: Pick<types.Comment, 'user' | 'createdAt'>
 }) {
+  const user = useContext(UserContext)
+
   return (
     <div className='flex items-center gap-16'>
-      <picture className='block w-32'>
-        <source
-          srcSet={`avatars/image-${info.user.username}.webp`}
-          type='image/webp'
-        />
-        <img
-          src={`avatars/image-${info.user.username}.png`}
-          alt={info.user.username}
-        />
-      </picture>
-      <span className='text-dark-blue font-medium'>{info.user.username}</span>
+      <div className='flex items-center'>
+        <picture className='block w-32 mr-16'>
+          <source
+            srcSet={`avatars/image-${info.user.username}.webp`}
+            type='image/webp'
+          />
+          <img
+            src={`avatars/image-${info.user.username}.png`}
+            alt={info.user.username}
+          />
+        </picture>
+        <span className='text-dark-blue font-medium'>{info.user.username}</span>
+        {info.user.username === user?.username && (
+          <span className='ml-8 text-sm text-white text-medium bg-moderate-blue rounded-sm pt-[1px] px-[6px] pb-[3px]'>
+            you
+          </span>
+        )}
+      </div>
       <span className='text-grayish-blue'>{info.createdAt}</span>
     </div>
   )
