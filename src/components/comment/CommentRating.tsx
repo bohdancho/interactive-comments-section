3fdd1f17bbe1b8473a9b6c648b46ee290b25dcbf
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-01-24 13:45:03 +0100

    implement static CommentRating

--- src/components/comment/CommentRating.tsx
+++ src/components/comment/CommentRating.tsx
@@ -1,6 +1,19 @@
-import { useReducer } from 'react'
 import * as types from '../../types'
+import iconPlus from '../../assets/icon-plus.svg'
+import iconMinus from '../../assets/icon-minus.svg'
 
 export function CommentRating({ rating }: Pick<types.Comment, 'rating'>) {
-  return <div className='rounded-large bg-very-light-gray'>+ {rating} -</div>
+  const getButton = (src: string, alt: string) => (
+    <button className='w-40 h-40 flex items-center justify-center'>
+      <img src={src} alt={alt} />
+    </button>
+  )
+
+  return (
+    <div className='inline-flex items-center gap-4 rounded-large bg-very-light-gray text-light-grayish-blue font-medium'>
+      {getButton(iconPlus, 'upvote')}
+      <span className='text-moderate-blue font-medium'>{rating}</span>
+      {getButton(iconMinus, 'downvote')}
+    </div>
+  )
 }
