4481e4f50007c82d976594fe65071caf1a44f205
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-02-20 20:02:00 +0100

    add action types

---
+++ src/types/actions.d.ts
@@ -0,0 +1,45 @@
+import { Data } from './data'
+
+interface InitAction {
+  type: 'init'
+  payload: Data
+}
+
+interface CommentAction {
+  type: 'comment'
+  payload: { text: string }
+}
+
+interface ReplyAction {
+  type: 'reply'
+  payload: { text: string; replyToComment: number }
+}
+
+interface DeleteCommentAction {
+  type: 'deleteComment'
+  payload: { id: number }
+}
+
+interface EditCommentAction {
+  type: 'editComment'
+  payload: { id: number; newText: string }
+}
+
+interface UpvoteAction {
+  type: 'upvote'
+  payload: { id: number }
+}
+
+interface DownvoteAction {
+  type: 'downvote'
+  payload: { id: number }
+}
+
+export type Action =
+  | InitAction
+  | CommentAction
+  | ReplyAction
+  | DeleteCommentAction
+  | EditCommentAction
+  | UpvoteAction
+  | DownvoteAction
