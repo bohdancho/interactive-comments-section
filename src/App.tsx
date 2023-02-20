086bf6403eaa9b44bb6f22a41f358cd64c24831d
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-02-20 20:11:24 +0100

    implement useReducer instead of useState and implement init action

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import * as types from './types'
-import { createContext, useEffect, useState } from 'react'
+import { Action } from './types'
+import { createContext, useEffect, useReducer } from 'react'
 import './App.css'
 import { CommentsSection } from './components/CommentsSection'
 
 export const UserContext = createContext<types.User | null>(null)
 const LS_DATA_KEY = 'data'
 
+const dataReducer = (state: null | types.Data, action: Action) => {
+  switch (action.type) {
+    case 'init':
+      return action.payload
+    default:
+      return state
+  }
+}
+
 function App() {
-  const [data, setData] = useState<types.Data | null>(null)
+  const [data, dispatchData] = useReducer(dataReducer, null)
 
   useEffect(() => {
     const localJSON = localStorage.getItem(LS_DATA_KEY)
     if (localJSON && JSON.parse(localJSON)) {
       const localData = JSON.parse(localJSON)
-      setData(localData)
+      dispatchData({ type: 'init', payload: localData })
       return
     }
 
     const dataFetch = async () => {
       const response = (await (await fetch('./data.json')).json()) as types.Data
 
-      setData(response)
+      dispatchData({ type: 'init', payload: response })
       localStorage.setItem(LS_DATA_KEY, JSON.stringify(response))
     }
 
