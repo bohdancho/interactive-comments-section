77c073b11e91541c207876703695f645c908d687
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-26 11:59:35 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+import eslint from 'vite-plugin-eslint'
+
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
     port: 4200,
   },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
-
