8afbc69dbc1656eb439251147b0cfc44aeb4d896
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-26 16:16:36 +0200

    Revert "Revert "setup basic dumb express ts backend""
    This reverts commit 9a8ed45ee194aa94a44529864d86f270e5ae8989.

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/index', (_req, res) => {
+  res.json({ message: 'Hello, index!' })
+})
+
+app.listen(port, () => {
+  console.log('Server listening on port', port)
+})
