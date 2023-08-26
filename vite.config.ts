8461875c6fa1b2bab2577c6c1779701c2c803674
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-26 15:48:45 +0200

    setup express backend with vite-plugin-mix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
+import mixPlugin, { Adapter } from 'vite-plugin-mix'
+const mix = (mixPlugin as unknown as Mix).default
 
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), eslint()],
+  plugins: [
+    mix({
+      handler: './src/api/handler.ts',
+    }),
+    react(),
+    eslint(),
+  ],
   server: {
     port: 4200,
   },
@@ -17,3 +25,14 @@ export default defineConfig({
     },
   },
 })
+
+interface MixConfig {
+  handler: string
+  adapter?: Adapter | undefined
+}
+
+type MixPlugin = (config: MixConfig) => Plugin
+
+interface Mix {
+  default: MixPlugin
+}
