58774dc2cd4d02bffcf050321ec890a8eaacddcd
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-09-01 16:02:47 +0200

    rm console.log

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -15,7 +15,6 @@ export class UserService {
   }
 
   async update(id: Types.ObjectId, payload: UpdateUserDto): Promise<UserDocument | null> {
-    console.log(payload)
     return await UserModel.findByIdAndUpdate(id, payload, { new: true })
   }
 
