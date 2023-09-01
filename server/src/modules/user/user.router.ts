e9bc9d2a5372529de2c5c3b2d057f8eafd9f131b
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-09-01 16:59:22 +0200

    add adminOnly token and validation for it

--- server/src/modules/user/user.router.ts
+++ server/src/modules/user/user.router.ts
@@ -1,14 +1,15 @@
-import { ObjectIdSchema, validateParams, validatePayload } from '@server/middleware'
+import { ObjectIdSchema, validateAdminOnly, validateParams, validatePayload } from '@server/middleware'
 import express from 'express'
 import { createUser, deleteUser, getAllUsers, getUser, updateUser } from './user.controller'
 import { CreateUserSchema, UpdateUserSchema } from './user.validation'
 
 const userRouter = express.Router()
 
-userRouter.post('/', validatePayload(CreateUserSchema), createUser)
 userRouter.get('/', getAllUsers)
 userRouter.get('/:id', validateParams(ObjectIdSchema), getUser)
-userRouter.put('/:id', validateParams(ObjectIdSchema), validatePayload(UpdateUserSchema), updateUser)
-userRouter.delete('/:id', validateParams(ObjectIdSchema), deleteUser)
+
+userRouter.post('/', validateAdminOnly, validatePayload(CreateUserSchema), createUser)
+userRouter.put('/:id', validateAdminOnly, validateParams(ObjectIdSchema), validatePayload(UpdateUserSchema), updateUser)
+userRouter.delete('/:id', validateAdminOnly, validateParams(ObjectIdSchema), deleteUser)
 
 export const userRouteMiddleware = userRouter
